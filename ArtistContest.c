25e2c8c54c133da139eaf038ee9975c3d2e89220
Stefanos Ventis <steveventis@gmail.com>
2026-02-12 22:37:35 +0200

    Artist Contest completed

--- ArtistContest.c
+++ ArtistContest.c
@@ -4,13 +4,6 @@
 #define NumberOfJudges 5
 #define NumberOfContestants 7
 
-
-boolean isCodeValid(int code);
-boolean isRatingValid(int rating); 
-void findMinMax(int ratings[], int arraySize, int* min, int* max);
-float calculateAverage(int ratings[], int arraySize);
-ContestResults findTheWinner(Contestant contestants[], int arraySize);
-
 typedef enum { 
     true, 
     false
@@ -26,7 +19,18 @@ typedef struct
 typedef struct 
 {
     Contestant* winners;
+    int winnersCount;
 } ContestResults;
+
+
+boolean isCodeValid(int code);
+boolean isRatingValid(int rating); 
+void findMinMax(int ratings[], int arraySize, int* min, int* max);
+float calculateAverage(int ratings[], int arraySize);
+ContestResults findTheWinner(Contestant contestants[], int arraySize);
+void printResults(Contestant contestants[], int arraySize, ContestResults results);
+
+
 
 
 int main(){
@@ -36,33 +40,53 @@ int main(){
     
     for(int i = 0; i < NumberOfContestants; i++){
         
-        printf("Κωδικός %dου Καλλιτέχνη(10-99): ", i + 1);
-        scanf("%d", &contestants[i].code);
+        do
+        {
+            printf("Κωδικός %dου Καλλιτέχνη(10-99): ", i + 1);
+            scanf("%d", &contestants[i].code);
+
+        } while (isCodeValid(contestants[i].code));
+        
+        
         for (int j = 0; j < NumberOfJudges; j++)
         {
-            printf("Βαθμός Κριτή %d", j + 1);
-            scanf("%d", &contestants[i].ratings[j]);
+            do
+            {
+                printf("Βαθμός Κριτή %d: ", j + 1);
+                scanf("%d", &contestants[i].ratings[j]);
+
+            } while (isRatingValid(contestants[i].ratings[j]));
+            
         }
 
         contestants[i].average = calculateAverage(contestants[i].ratings, NumberOfJudges);
     }
 
+    ContestResults results = findTheWinner(contestants, NumberOfContestants);
+
+    printResults(contestants, NumberOfContestants, results);
     
-
-    // for (int i = 0; i < NumberOfContestants; i++)
-    // {
-    //     printf("%d\n",contestants[i].code);
-    // }
-
-    return 0;
+    free(results.winners);
 }
 
 boolean isCodeValid(int code){
-    return false;
+    if (code < 10 || code > 99)
+    {
+        printf("Λάθος τιμή!\n");
+        return false;
+    }
+    
+    return true;
 }
 
 boolean isRatingValid(int rating){
-    return false;
+    if (rating > 10 || rating < 0)
+    {
+        printf("Λάθος τιμή!\n");
+        return false;
+    }
+    
+    return true;
 }
 
 float calculateAverage(int ratings[], int arraySize){
@@ -93,11 +117,68 @@ void findMinMax(int ratings[], int arraySize, int *min, int *max){
     }
 }
 
-
 ContestResults findTheWinner(Contestant contestants[], int arraySize){
     ContestResults results;
     results.winners = NULL;
+    results.winnersCount = 0;
+
+    float maximumScore = contestants[0].average;
     
+    Contestant* temp = malloc(sizeof(Contestant) * arraySize);
+    int counter = 0;
+    
+    for (int i = 0; i < arraySize; i++)
+    {
+        if (contestants[i].average > maximumScore)
+        {
+            maximumScore = contestants[i].average;
+            counter = 0;
+            temp[counter++] = contestants[i];
+        }
+        else if (contestants[i].average == maximumScore)
+        {
+            temp[counter++] = contestants[i];
+        }   
+    }
+    
+    results.winners = malloc(sizeof(Contestant) * counter);
+    results.winnersCount = counter;
+
+    for (int i = 0; i < counter; i++)
+    {
+        results.winners[i] = temp[i];
+    }
+    
+    free(temp);
 
     return results;
+}
+
+void printResults(Contestant contestants[], int arraySize, ContestResults results){
+    printf("\n--- ΑΠΟΤΕΛΕΣΜΑΤΑ ΔΙΑΓΩΝΙΣΜΟΥ ΤΡΑΓΟΥΔΙΟΥ ---\n");
+
+    printf("%-4s| %-3s| %-3s| %-3s| %-3s| %-3s| %-6s\n",
+           "ΚΚ", "Κ1", "Κ2", "Κ3", "Κ4", "Κ5", "Μ.Ο.");
+    printf("----|----|----|----|----|----|--------\n");
+
+    for (int i = 0; i < arraySize; i++)
+    {
+        printf("%-4d| %-3d| %-3d| %-3d| %-3d| %-3d| %-6.2f\n",
+               contestants[i].code,
+               contestants[i].ratings[0],
+               contestants[i].ratings[1],
+               contestants[i].ratings[2],
+               contestants[i].ratings[3],
+               contestants[i].ratings[4],
+               contestants[i].average);
+    }
+
+    printf("\n");
+
+    for (int i = 0; i < results.winnersCount; i++)
+    {
+        printf("ΝΙΚΗΤΗΣ: Καλλιτέχνης με ΚΚ = %d (Μ.Ο. = %.2f)\n",
+               results.winners[i].code,
+               results.winners[i].average);
+    }
 }
